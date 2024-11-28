a7417fec3577a9c1f62dd48ad72c4692f13e6dfa
KIMSEI1124 <workju1124@gmail.com>
2024-11-28 19:21:32 +0900

    Feat: add Repository

--- java.sh
+++ java.sh
@@ -13,7 +13,7 @@ log() {
   echo "${BOLD}${BLUE}($step/$total_steps)${RESET} ${BOLD}${GREEN}[$(date +"%Y-%m-%d %H:%M:%S")]${RESET} $message"
 }
 
-TOTAL_STEPS=5
+TOTAL_STEPS=6
 
 log 1 $TOTAL_STEPS "📝 Selecting Java version for installation"
 echo "Available Java versions for Amazon Corretto:"
@@ -35,31 +35,39 @@ case $version_choice in
 esac
 log 1 $TOTAL_STEPS "✅ Selected Java version: $JAVA_VERSION"
 
-log 2 $TOTAL_STEPS "📦 Updating package index"
+log 2 $TOTAL_STEPS "🌐 Adding Amazon Corretto repository"
+curl -fsSL https://apt.corretto.aws/corretto.key | sudo gpg --dearmor -o /etc/apt/keyrings/corretto.gpg
+echo "deb [signed-by=/etc/apt/keyrings/corretto.gpg arch=amd64] https://apt.corretto.aws stable main" | sudo tee /etc/apt/sources.list.d/corretto.list > /dev/null
+if [ $? -ne 0 ]; then
+  log 2 $TOTAL_STEPS "❌ Failed to add Amazon Corretto repository. Please check your internet connection." $RED
+  exit 1
+fi
+
+log 3 $TOTAL_STEPS "📦 Updating package index"
 sudo apt-get update
 if [ $? -ne 0 ]; then
-  log 2 $TOTAL_STEPS "❌ Failed to update package index. Please check your internet connection." $RED
+  log 3 $TOTAL_STEPS "❌ Failed to update package index. Please check your internet connection." $RED
   exit 1
 fi
 
-log 3 $TOTAL_STEPS "🚀 Installing $JAVA_VERSION"
+log 4 $TOTAL_STEPS "🚀 Installing $JAVA_VERSION"
 sudo apt-get install -y $JAVA_VERSION
 if [ $? -ne 0 ]; then
-  log 3 $TOTAL_STEPS "❌ Failed to install $JAVA_VERSION. Please check your repository settings." $RED
+  log 4 $TOTAL_STEPS "❌ Failed to install $JAVA_VERSION. Please check your repository settings." $RED
   exit 1
 fi
-log 3 $TOTAL_STEPS "✅ Successfully installed $JAVA_VERSION"
+log 4 $TOTAL_STEPS "✅ Successfully installed $JAVA_VERSION"
 
-log 4 $TOTAL_STEPS "🔍 Verifying Java installation"
+log 5 $TOTAL_STEPS "🔍 Verifying Java installation"
 java -version
 if [ $? -ne 0 ]; then
-  log 4 $TOTAL_STEPS "❌ Java installation verification failed. Please troubleshoot." $RED
+  log 5 $TOTAL_STEPS "❌ Java installation verification failed. Please troubleshoot." $RED
   exit 1
 fi
-log 4 $TOTAL_STEPS "✅ Java installation verified successfully"
+log 5 $TOTAL_STEPS "✅ Java installation verified successfully"
 
-log 5 $TOTAL_STEPS "⚙️ Setting default Java version"
+log 6 $TOTAL_STEPS "⚙️ Setting default Java version"
 sudo update-alternatives --config java
-log 5 $TOTAL_STEPS "✅ Default Java version set successfully"
+log 6 $TOTAL_STEPS "✅ Default Java version set successfully"
 
 log $TOTAL_STEPS $TOTAL_STEPS "🎉 Java setup completed successfully!"
